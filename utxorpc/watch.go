a3e9e3048bd1b8172453ebc6f58a02c0bbd480bb
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-02-01 14:32:14 -0500

    feat: filter watchtx on predicates
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- utxorpc/watch.go
+++ utxorpc/watch.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Blink Labs Software
+// Copyright 2025 Blink Labs Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,12 +15,14 @@
 package utxorpc
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 
 	"connectrpc.com/connect"
 	"github.com/blinklabs-io/gouroboros/ledger"
 	ocommon "github.com/blinklabs-io/gouroboros/protocol/common"
+	cardano "github.com/utxorpc/go-codegen/utxorpc/v1alpha/cardano"
 	watch "github.com/utxorpc/go-codegen/utxorpc/v1alpha/watch"
 	"github.com/utxorpc/go-codegen/utxorpc/v1alpha/watch/watchconnect"
 )
@@ -142,10 +144,163 @@ func (s *watchServiceServer) WatchTx(
 						return err
 					}
 				} else {
-					// TODO: filter from all Predicate types
-					err := stream.Send(resp)
-					if err != nil {
-						return err
+					found := false
+					assetFound := false
+
+					// Check Predicate
+					addressPattern := predicate.GetMatch().GetCardano().GetHasAddress()
+					mintAssetPattern := predicate.GetMatch().GetCardano().GetMintsAsset()
+					moveAssetPattern := predicate.GetMatch().GetCardano().GetMovesAsset()
+
+					var addresses []ledger.Address
+					if addressPattern != nil {
+						// Handle Exact Address
+						exactAddressBytes := addressPattern.GetExactAddress()
+						if exactAddressBytes != nil {
+							var addr ledger.Address
+							err := addr.UnmarshalCBOR(exactAddressBytes)
+							if err != nil {
+								return fmt.Errorf(
+									"failed to decode exact address: %w",
+									err,
+								)
+							}
+							addresses = append(addresses, addr)
+						}
+
+						// Handle Payment Part
+						paymentPart := addressPattern.GetPaymentPart()
+						if paymentPart != nil {
+							s.utxorpc.config.Logger.Info("PaymentPart is present, decoding...")
+							var paymentAddr ledger.Address
+							err := paymentAddr.UnmarshalCBOR(paymentPart)
+							if err != nil {
+								return fmt.Errorf("failed to decode payment part: %w", err)
+							}
+							addresses = append(addresses, paymentAddr)
+						}
+
+						// Handle Delegation Part
+						delegationPart := addressPattern.GetDelegationPart()
+						if delegationPart != nil {
+							s.utxorpc.config.Logger.Info(
+								"DelegationPart is present, decoding...",
+							)
+							var delegationAddr ledger.Address
+							err := delegationAddr.UnmarshalCBOR(delegationPart)
+							if err != nil {
+								return fmt.Errorf(
+									"failed to decode delegation part: %w",
+									err,
+								)
+							}
+							addresses = append(addresses, delegationAddr)
+						}
+					}
+
+					var assetPatterns []*cardano.AssetPattern
+					if mintAssetPattern != nil {
+						assetPatterns = append(assetPatterns, mintAssetPattern)
+					}
+					if moveAssetPattern != nil {
+						assetPatterns = append(assetPatterns, moveAssetPattern)
+					}
+
+					// Convert everything to utxos (ledger.TransactionOutput) for matching
+					var utxos []ledger.TransactionOutput
+					utxos = append(tx.Outputs(), tx.CollateralReturn())
+					var inputs []ledger.TransactionInput
+					inputs = append(tx.Inputs(), tx.ReferenceInputs()...)
+					inputs = append(inputs, tx.Collateral()...)
+					for _, input := range inputs {
+						utxo, err := s.utxorpc.config.LedgerState.UtxoByRef(
+							input.Id().Bytes(),
+							input.Index(),
+						)
+						if err != nil {
+							return fmt.Errorf(
+								"failed to look up input: %w",
+								err,
+							)
+						}
+						ret, err := utxo.Decode() // ledger.TransactionOutput
+						if err != nil {
+							return err
+						}
+						if ret == nil {
+							return fmt.Errorf("decode returned empty utxo")
+						}
+						utxos = append(utxos, ret)
+					}
+
+					// Check UTxOs for addresses
+					for _, address := range addresses {
+						if found {
+							break
+						}
+						if assetFound {
+							found = true
+							break
+						}
+						for _, utxo := range utxos {
+							if found {
+								break
+							}
+							if assetFound {
+								found = true
+								break
+							}
+							if utxo.Address().String() == address.String() {
+								if found {
+									break
+								}
+								if assetFound {
+									found = true
+									break
+								}
+								// We matched address, check assetPatterns
+								for _, assetPattern := range assetPatterns {
+									// Address found, no assetPattern
+									if assetPattern == nil {
+										found = true
+										break
+									}
+									// Filter on assetPattern
+									for _, policyId := range utxo.Assets().Policies() {
+										if assetFound {
+											found = true
+											break
+										}
+										if bytes.Equal(
+											policyId.Bytes(),
+											assetPattern.PolicyId,
+										) {
+											for _, asset := range utxo.Assets().Assets(policyId) {
+												if bytes.Equal(asset, assetPattern.AssetName) {
+													found = true
+													assetFound = true
+													break
+												}
+											}
+										}
+									}
+								}
+								if found {
+									break
+								}
+								// Asset not found; skip this UTxO
+								if !assetFound {
+									continue
+								}
+								found = true
+							}
+						}
+					}
+					if found {
+						err := stream.Send(resp)
+						if err != nil {
+							return err
+						}
 					}
 				}
 			}
