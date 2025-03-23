f6eb163df38abbe304d67b4767efbf03e47230ab
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-03-23 14:56:34 -0400

    refactor(database): GetEpochLatest funcs in database (#511)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

---
+++ database/epoch.go
@@ -0,0 +1,54 @@
+// Copyright 2025 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package database
+
+type Epoch struct {
+	ID uint `gorm:"primarykey"`
+	// NOTE: we would normally use this as the primary key, but GORM doesn't
+	// like a primary key value of 0
+	EpochId       uint64 `gorm:"uniqueIndex"`
+	StartSlot     uint64
+	Nonce         []byte
+	EraId         uint
+	SlotLength    uint
+	LengthInSlots uint
+}
+
+func (Epoch) TableName() string {
+	return "epoch"
+}
+
+func GetEpochLatest(db *Database) (Epoch, error) {
+	return db.GetEpochLatest(nil)
+}
+
+func (d *Database) GetEpochLatest(txn *Txn) (Epoch, error) {
+	tmpEpoch := Epoch{}
+	if txn == nil {
+		txn = d.Transaction(false)
+	}
+	epoch, err := txn.DB().Metadata().GetEpochLatest(txn.Metadata())
+	if err != nil {
+		return tmpEpoch, err
+	}
+	tmpEpoch.ID = epoch.ID
+	tmpEpoch.EpochId = epoch.EpochId
+	tmpEpoch.StartSlot = epoch.StartSlot
+	tmpEpoch.Nonce = epoch.Nonce
+	tmpEpoch.EraId = epoch.EraId
+	tmpEpoch.SlotLength = epoch.SlotLength
+	tmpEpoch.LengthInSlots = epoch.LengthInSlots
+	return tmpEpoch, nil
+}
