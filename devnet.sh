c2f31249bb6e0bfa6944285ca011acacf81a33a8
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-08-01 16:44:50 -0400

    fix: update start time when starting devnet (#817)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- devnet.sh
+++ devnet.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 export CARDANO_NETWORK=devnet
-export CARDANO_CONFIG=./config/cardano/${CARDANO_NETWORK}/config.json
+export CARDANO_CONFIG=./config/cardano/devnet/config.json
 export CARDANO_DATABASE_PATH=.devnet
 export CARDANO_DEV_MODE=true
+
+conf=$(dirname $CARDANO_CONFIG)
+now=$(date -u +%s)
+echo setting start time in $conf to $now
+sed -i -e "s/startTime\": .*,/startTime\": $now,/" $conf/byron-genesis.json
+sed -i -e "s/systemStart\": .*,/systemStart\": \"$(date -u +%Y-%m-%dT%H:%M:%SZ --date=@$now)\",/" $conf/shelley-genesis.json
 
 echo resetting .devnet
 rm -rf .devnet/*
