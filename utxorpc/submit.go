c2f8e0e46b2f2deafa6bed85dd63c5e43b48af90
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-11-17 14:25:58 -0500

    chore(deps): upgrade gouroboros to 0.139.0 (#1025)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- utxorpc/submit.go
+++ utxorpc/submit.go
@@ -17,9 +17,11 @@ package utxorpc
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"math"
 
 	"connectrpc.com/connect"
 	"github.com/blinklabs-io/dingo/event"
@@ -42,86 +44,33 @@ func (s *submitServiceServer) SubmitTx(
 	ctx context.Context,
 	req *connect.Request[submit.SubmitTxRequest],
 ) (*connect.Response[submit.SubmitTxResponse], error) {
-	txRawList := req.Msg.GetTx() // []*AnyChainTx
+	txRaw := req.Msg.GetTx()
 
-	s.utxorpc.config.Logger.Info(
-		fmt.Sprintf(
-			"Got a SubmitTx request with %d transactions",
-			len(txRawList),
-		),
-	)
+	s.utxorpc.config.Logger.Info("Got a SubmitTx request")
 	resp := &submit.SubmitTxResponse{}
 
-	// Loop through the transactions and add each to the mempool
-	errorList := make([]error, len(txRawList))
-	hasError := false
-	placeholderRef := []byte{}
-	for i, txi := range txRawList {
-		txRawBytes := txi.GetRaw() // raw bytes
-		txType, err := gledger.DetermineTransactionType(txRawBytes)
-		if err != nil {
-			resp.Ref = append(resp.Ref, placeholderRef)
-			errorList[i] = err
-			s.utxorpc.config.Logger.Error(
-				fmt.Sprintf(
-					"failed decoding tx %d: %v",
-					i,
-					err,
-				),
-			)
-			hasError = true
-			continue
-		}
-		tx, err := gledger.NewTransactionFromCbor(txType, txRawBytes)
-		if err != nil {
-			resp.Ref = append(resp.Ref, placeholderRef)
-			errorList[i] = err
-			s.utxorpc.config.Logger.Error(
-				fmt.Sprint(
-					fmt.Errorf(
-						"failed to decode transaction from CBOR: %w",
-						err,
-					),
-				),
-			)
-			hasError = true
-			continue
-		}
-		if tx == nil {
-			resp.Ref = append(resp.Ref, placeholderRef)
-			errorList[i] = errors.New("decoded transaction is nil")
-			s.utxorpc.config.Logger.Error("decoded transaction is nil")
-			hasError = true
-			continue
-		}
-		txHash := tx.Hash()
-		// Add transaction to mempool
-		err = s.utxorpc.config.Mempool.AddTransaction(txType, txRawBytes)
-		if err != nil {
-			resp.Ref = append(resp.Ref, placeholderRef)
-			errorList[i] = fmt.Errorf("%s", err.Error())
-			s.utxorpc.config.Logger.Error(
-				fmt.Sprintf(
-					"failed to add tx %s to mempool: %s",
-					txHash.String(),
-					err,
-				),
-			)
-			hasError = true
-			continue
-		}
-		if err != nil {
-			resp.Ref = append(resp.Ref, placeholderRef)
-			errorList[i] = err
-			hasError = true
-			continue
-		}
-		resp.Ref = append(resp.Ref, txHash.Bytes())
+	txRawBytes := txRaw.GetRaw()
+	txType, err := gledger.DetermineTransactionType(txRawBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed decoding tx: %w", err)
 	}
-	if hasError {
-		return connect.NewResponse(resp), fmt.Errorf("%v", errorList)
+	tx, err := gledger.NewTransactionFromCbor(txType, txRawBytes)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to decode transaction from CBOR: %w",
+			err,
+		)
 	}
-
+	if tx == nil {
+		return nil, errors.New("decoded transaction is nil")
+	}
+	txHash := tx.Hash()
+	// Add transaction to mempool
+	err = s.utxorpc.config.Mempool.AddTransaction(txType, txRawBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to add tx to mempool: %w", err)
+	}
+	resp.Ref = txHash.Bytes()
 	return connect.NewResponse(resp), nil
 }
 
@@ -188,69 +137,81 @@ func (s *submitServiceServer) EvalTx(
 	req *connect.Request[submit.EvalTxRequest],
 ) (*connect.Response[submit.EvalTxResponse], error) {
 	s.utxorpc.config.Logger.Info("Got an EvalTx request")
-	txRawList := req.Msg.GetTx() // []*AnyChainTx
+	txRaw := req.Msg.GetTx()
 	resp := &submit.EvalTxResponse{}
-	for _, txi := range txRawList {
-		txRawBytes := txi.GetRaw()
-		// Decode TX
-		txType, err := gledger.DetermineTransactionType(txRawBytes)
-		if err != nil {
-			return nil, fmt.Errorf(
-				"could not parse transaction to determine type: %w",
-				err,
-			)
-		}
-		tx, err := gledger.NewTransactionFromCbor(txType, txRawBytes)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse transaction CBOR: %w", err)
-		}
-		// Evaluate TX
-		fee, totalExUnits, redeemerExUnits, err := s.utxorpc.config.LedgerState.EvaluateTx(
-			tx,
+
+	txRawBytes := txRaw.GetRaw()
+	// Decode TX
+	txType, err := gledger.DetermineTransactionType(txRawBytes)
+	if err != nil {
+		return nil, fmt.Errorf(
+			"could not parse transaction to determine type: %w",
+			err,
 		)
-		// Populate response
-		tmpRedeemers := make([]*cardano.Redeemer, 0, len(redeemerExUnits))
-		for key, val := range redeemerExUnits {
-			tmpRedeemers = append(
-				tmpRedeemers,
-				&cardano.Redeemer{
-					Purpose: cardano.RedeemerPurpose(key.Tag),
-					Index:   key.Index,
-					ExUnits: &cardano.ExUnits{
-						Steps:  uint64(val.Steps),  // nolint:gosec
-						Memory: uint64(val.Memory), // nolint:gosec
-					},
-					// TODO: Payload
+	}
+	tx, err := gledger.NewTransactionFromCbor(txType, txRawBytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse transaction CBOR: %w", err)
+	}
+	// Evaluate TX
+	fee, totalExUnits, redeemerExUnits, err := s.utxorpc.config.LedgerState.EvaluateTx(
+		tx,
+	)
+	// Populate response
+	tmpRedeemers := make([]*cardano.Redeemer, 0, len(redeemerExUnits))
+	for key, val := range redeemerExUnits {
+		tmpRedeemers = append(
+			tmpRedeemers,
+			&cardano.Redeemer{
+				Purpose: cardano.RedeemerPurpose(key.Tag),
+				Index:   key.Index,
+				ExUnits: &cardano.ExUnits{
+					Steps:  uint64(val.Steps),  // nolint:gosec
+					Memory: uint64(val.Memory), // nolint:gosec
 				},
-			)
+				// TODO: Payload
+			},
+		)
+	}
+	var txEval *cardano.TxEval
+	if err != nil {
+		txEval = &cardano.TxEval{
+			Errors: []*cardano.EvalError{
+				{
+					Msg: err.Error(),
+				},
+			},
 		}
-		var txEval *cardano.TxEval
-		if err != nil {
-			txEval = &cardano.TxEval{
-				Errors: []*cardano.EvalError{
-					{
-						Msg: err.Error(),
-					},
+	} else {
+		var feeBigInt *cardano.BigInt
+		if fee <= math.MaxInt64 {
+			feeBigInt = &cardano.BigInt{
+				BigInt: &cardano.BigInt_Int{
+					Int: int64(fee),
 				},
 			}
 		} else {
-			txEval = &cardano.TxEval{
-				Fee: fee,
-				ExUnits: &cardano.ExUnits{
-					Steps:  uint64(totalExUnits.Steps),  // nolint:gosec
-					Memory: uint64(totalExUnits.Memory), // nolint:gosec
+			buf := make([]byte, 8)
+			binary.BigEndian.PutUint64(buf, fee)
+			feeBigInt = &cardano.BigInt{
+				BigInt: &cardano.BigInt_BigUInt{
+					BigUInt: buf,
 				},
-				Redeemers: tmpRedeemers,
 			}
 		}
-		resp.Report = append(
-			resp.Report,
-			&submit.AnyChainEval{
-				Chain: &submit.AnyChainEval_Cardano{
-					Cardano: txEval,
-				},
+		txEval = &cardano.TxEval{
+			Fee: feeBigInt,
+			ExUnits: &cardano.ExUnits{
+				Steps:  uint64(totalExUnits.Steps),  // nolint:gosec
+				Memory: uint64(totalExUnits.Memory), // nolint:gosec
 			},
-		)
+			Redeemers: tmpRedeemers,
+		}
+	}
+	resp.Report = &submit.AnyChainEval{
+		Chain: &submit.AnyChainEval_Cardano{
+			Cardano: txEval,
+		},
 	}
 	return connect.NewResponse(resp), nil
 }
