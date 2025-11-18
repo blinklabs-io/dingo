04494612ea124391a85b8f6e03ca18f2ed1e63b3
Akhil <145306795+arepala-uml@users.noreply.github.com>
2025-11-17 20:47:49 -0600

    feat(aws): Implemented S3-backed blob storage with key-value interface (#1014)
    * feat(aws): Implemented S3-backed blob storage with key-value interface
    
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>
    
    * feat(aws): Fixed the log messages to overcome golangcilint errors
    
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>
    
    * feat(aws): Fixed the log messages to overcome golangcilint errors
    
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>
    
    * feat(aws): Fixed the nailway workflow failures
    
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>
    
    * feat(aws): Fixed the coderabbit comments
    
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>
    
    * feat(aws): Fixed the case sensitivity mismatch in prefix handling
    
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>
    
    ---------
    
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>

---
+++ database/plugin/blob/aws/commit_timestamp.go
@@ -0,0 +1,35 @@
+// Copyright 2025 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aws
+
+import (
+	"context"
+	"math/big"
+)
+
+const commitTimestampBlobKey = "metadata_commit_timestamp"
+
+func (b *BlobStoreS3) GetCommitTimestamp(ctx context.Context) (int64, error) {
+	data, err := b.Get(ctx, commitTimestampBlobKey)
+	if err != nil {
+		return 0, err
+	}
+	return new(big.Int).SetBytes(data).Int64(), nil
+}
+
+func (b *BlobStoreS3) SetCommitTimestamp(ctx context.Context, ts int64) error {
+	raw := new(big.Int).SetInt64(ts).Bytes()
+	return b.Put(ctx, commitTimestampBlobKey, raw)
+}
