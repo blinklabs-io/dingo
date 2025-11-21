b9770b097a6aae29328eb9a1011aa9233d5c6787
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-11-21 17:36:57 -0500

    fix(peergov): initial peers and guard against nil
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- peergov/peergov.go
+++ peergov/peergov.go
@@ -55,6 +55,7 @@ func NewPeerGovernor(cfg PeerGovernorConfig) *PeerGovernor {
 	cfg.Logger = cfg.Logger.With("component", "peergov")
 	return &PeerGovernor{
 		config: cfg,
+		peers:  []*Peer{},
 	}
 }
 
@@ -84,6 +85,9 @@ func (p *PeerGovernor) LoadTopologyConfig(
 	// Remove peers originally sourced from the topology
 	tmpPeers := []*Peer{}
 	for _, tmpPeer := range p.peers {
+		if tmpPeer == nil {
+			continue
+		}
 		if tmpPeer.Source == PeerSourceTopologyBootstrapPeer ||
 			tmpPeer.Source == PeerSourceTopologyLocalRoot ||
 			tmpPeer.Source == PeerSourceTopologyPublicRoot {
