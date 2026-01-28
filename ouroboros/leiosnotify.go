8aada3e9bf520ce75cdd67fc62b5380a2bf331f9
Aurora Gaffney <aurora@blinklabs.io>
2026-01-28 15:45:06 -0500

    feat: initial Leios support
    Signed-off-by: Aurora Gaffney <aurora@blinklabs.io>

---
+++ ouroboros/leiosnotify.go
@@ -0,0 +1,39 @@
+// Copyright 2026 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ouroboros
+
+import (
+	"github.com/blinklabs-io/gouroboros/protocol"
+	oleiosnotify "github.com/blinklabs-io/gouroboros/protocol/leiosnotify"
+)
+
+func (o *Ouroboros) leiosnotifyServerConnOpts() []oleiosnotify.LeiosNotifyOptionFunc {
+	return []oleiosnotify.LeiosNotifyOptionFunc{
+		oleiosnotify.WithRequestNextFunc(o.leiosnotifyServerRequestNext),
+	}
+}
+
+func (o *Ouroboros) leiosnotifyClientConnOpts() []oleiosnotify.LeiosNotifyOptionFunc {
+	return []oleiosnotify.LeiosNotifyOptionFunc{
+		// NOTE: this is purposely empty
+	}
+}
+
+func (o *Ouroboros) leiosnotifyServerRequestNext(
+	ctx oleiosnotify.CallbackContext,
+) (protocol.Message, error) {
+	// TODO
+	return nil, nil
+}
