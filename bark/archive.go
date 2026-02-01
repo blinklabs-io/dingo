59e12b75655c0e6a2214abb5648133d1a67cfe05
Chris Guiney <chris@guiney.net>
2026-02-01 11:09:52 -0800

    chore(bark): add package alias to try and satisfy goimports linter
    Signed-off-by: Chris Guiney <chris@guiney.net>

--- bark/archive.go
+++ bark/archive.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"connectrpc.com/connect"
-	"github.com/blinklabs-io/bark/proto/v1alpha1/archive"
+	archive "github.com/blinklabs-io/bark/proto/v1alpha1/archive"
 	"github.com/blinklabs-io/bark/proto/v1alpha1/archive/archivev1alpha1connect"
 	"github.com/blinklabs-io/dingo/database"
 	"github.com/blinklabs-io/gouroboros/protocol/common"
@@ -17,8 +17,8 @@ type archiveServiceHandler struct {
 	bark *Bark
 }
 
-func (a *archiveServiceHandler) FetchBlock(ctx context.Context, req *connect.Request[archivev1alpha1.FetchBlockRequest]) (*connect.Response[archivev1alpha1.FetchBlockResponse], error) {
-	resp := &connect.Response[archivev1alpha1.FetchBlockResponse]{}
+func (a *archiveServiceHandler) FetchBlock(ctx context.Context, req *connect.Request[archive.FetchBlockRequest]) (*connect.Response[archive.FetchBlockResponse], error) {
+	resp := &connect.Response[archive.FetchBlockResponse]{}
 
 	for _, b := range req.Msg.GetBlocks() {
 		point := common.NewPoint(b.GetSlot(), []byte(b.GetHash()))
@@ -26,10 +26,10 @@ func (a *archiveServiceHandler) FetchBlock(ctx context.Context, req *connect.Req
 		if err != nil {
 			return nil, fmt.Errorf("failed getting signed url for block %v: %w", point, err)
 		}
-		resp.Msg.Blocks = append(resp.Msg.Blocks, &archivev1alpha1.SignedUrl{
+		resp.Msg.Blocks = append(resp.Msg.Blocks, &archive.SignedUrl{
 			Url: u.String(),
 		})
 	}
 
-	return connect.NewResponse[archivev1alpha1.FetchBlockResponse](nil), nil
+	return connect.NewResponse[archive.FetchBlockResponse](nil), nil
 }
