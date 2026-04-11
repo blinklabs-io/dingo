cbde9ca5ab1efac32f42ebece9e6ea703345e6d7
Akhil Repala <arepala@blinklabs.io>
2026-04-11 13:23:48 -0500

    fix(blockfrost): fixed account active epoch lookup errors
    Signed-off-by: Akhil Repala <arepala@blinklabs.io>

--- blockfrost/adapter.go
+++ blockfrost/adapter.go
@@ -587,6 +587,14 @@ func (a *NodeAdapter) Account(
 			err,
 		)
 	}
+	epochID, err := uint64ToInt64(
+		epoch.EpochId,
+		"account active epoch",
+	)
+	if err != nil {
+		return AccountInfo{}, err
+	}
+	activeEpoch = &epochID
 
 	epochID, err := uint64ToInt64(
 		epoch.EpochId,
