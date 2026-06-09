1690faaf767c192c1b0694641a8e6b7424882cb0
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-06-09 11:56:50 -0400

    docs(examples): blockfrost explorer glow up
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- examples/dingo-blockfrost-explorer/scripts/run-dev.sh
+++ examples/dingo-blockfrost-explorer/scripts/run-dev.sh
@@ -2,5 +2,6 @@
 set -eu
 
 export DINGO_BLOCKFROST_URL="${DINGO_BLOCKFROST_URL:-http://127.0.0.1:3000}"
+export DINGO_METRICS_URL="${DINGO_METRICS_URL:-http://127.0.0.1:12798}"
 
 exec npm run dev -- --host 0.0.0.0
