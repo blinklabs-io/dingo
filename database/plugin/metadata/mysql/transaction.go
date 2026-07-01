5f342b7c10a3556e79e6f7e7241e7334f3763f45
cryptodj413 <shinjirohara2@gmail.com>
2026-07-01 22:43:23 +0200

    build(database): gate optional metadata plugins behind dingo_extra_plugins
    Signed-off-by: cryptodj413 <shinjirohara2@gmail.com>

--- database/plugin/metadata/mysql/transaction.go
+++ database/plugin/metadata/mysql/transaction.go
@@ -12,6 +12,8 @@
 // either express or implied. See the License for the specific language
 // governing permissions and limitations under the License.
 
+//go:build dingo_extra_plugins
+
 package mysql
 
 import (
