115d6e5bb1eebd195d81a625d562611516bd1224
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-08-17 17:37:22 -0400

    test: define Koios parity coverage
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- internal/koiosparity/koios_client_test.go
+++ internal/koiosparity/koios_client_test.go
@@ -245,6 +245,64 @@ func TestGetPoolFirstActiveEpochsTakesMinAcrossUpdates(t *testing.T) {
 	require.False(t, ok)
 }
 
+func TestFilteredKoiosResponsesRequireRequestedEpoch(t *testing.T) {
+	tests := []struct {
+		name string
+		path string
+		call func(*KoiosClient) error
+	}{
+		{
+			name: "epoch_info",
+			path: "/epoch_info",
+			call: func(k *KoiosClient) error {
+				_, err := k.GetEpochInfo(context.Background(), 10)
+				return err
+			},
+		},
+		{
+			name: "totals",
+			path: "/totals",
+			call: func(k *KoiosClient) error {
+				_, err := k.GetTotals(context.Background(), 10)
+				return err
+			},
+		},
+		{
+			name: "pool_history",
+			path: "/pool_history",
+			call: func(k *KoiosClient) error {
+				_, err := k.GetPoolEpochHistory(context.Background(), "pool1test", 10)
+				return err
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				require.Equal(t, test.path, r.URL.Path)
+				w.WriteHeader(http.StatusOK)
+				_, _ = w.Write([]byte(`[{"epoch_no":11}]`))
+			}))
+			defer srv.Close()
+
+			err := test.call(newTestKoiosClient(srv.URL))
+			require.ErrorContains(t, err, "requested epoch 10")
+		})
+	}
+}
+
+func TestFilteredKoiosResponsesRejectMultipleRows(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(`[{"epoch_no":10},{"epoch_no":10}]`))
+	}))
+	defer srv.Close()
+
+	_, err := newTestKoiosClient(srv.URL).GetEpochInfo(context.Background(), 10)
+	require.ErrorContains(t, err, "got 2 row(s)")
+}
+
 func TestRationalsEqual(t *testing.T) {
 	require.True(t, rationalsEqual("0.1", "1/10"))
 	require.True(t, rationalsEqual("1/20", "0.05"))
