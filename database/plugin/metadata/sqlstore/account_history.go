6a86cea4a924ed5ef09365f45bb80571c8cd2076
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-04 17:25:32 -0400

    fix(database): preserve text registration deposits
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- database/plugin/metadata/sqlstore/account_history.go
+++ database/plugin/metadata/sqlstore/account_history.go
@@ -731,7 +731,7 @@ SELECT %[1]s.added_slot AS added_slot,
        certs.cert_index AS cert_index,
        tx.hash AS tx_hash,
        ? AS action,
-       COALESCE(%[2]s, 0) AS deposit,
+       COALESCE(%[2]s, '0') AS deposit,
        tx.slot AS tx_slot,
        tx.block_hash AS block_hash
 FROM %[1]s
