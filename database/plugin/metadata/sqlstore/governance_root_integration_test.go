cf67efff83a8e3de91e17f3362111f65672d27f3
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-09-26 12:48:49 -0400

    test(database): cover same-boundary governance root lookup
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

---
+++ database/plugin/metadata/sqlstore/governance_root_integration_test.go
@@ -0,0 +1,37 @@
+//go:build dingo_db_integration
+
+// Copyright 2026 Blink Labs Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sqlstore
+
+import "testing"
+
+// The root lookup's correlated NOT EXISTS subquery references the outer
+// governance_proposal row by table name; run it on each real backend.
+func TestPostgresLastEnactedGovernanceRootIntegration(t *testing.T) {
+	dsn, schema := newPostgresIntegrationSchema(t)
+	exerciseLastEnactedGovernanceRoot(
+		t,
+		newIntegrationSQLStore(t, "pgx", dsn, "postgres", schema),
+	)
+}
+
+func TestMySQLLastEnactedGovernanceRootIntegration(t *testing.T) {
+	dsn, database := newMySQLIntegrationDatabase(t)
+	exerciseLastEnactedGovernanceRoot(
+		t,
+		newIntegrationSQLStore(t, "mysql", dsn, "mysql", database),
+	)
+}
